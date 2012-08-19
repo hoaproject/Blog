f6e8b174b885553b982e10088aec024fbd828336
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2012-08-19 19:24:34 +0200

    Remove copy/pasted comments and debug and force default status to 302 for redirect kit.

--- Application/Kit/Redirect.php
+++ Application/Kit/Redirect.php
@@ -12,15 +12,10 @@ namespace Application\Kit {
     class Redirect extends \Hoa\Dispatcher\Kit {
 
       public function redirect ( $ruleId, Array $data, $secured = null,
-                                 $status = null ) {
+                                 $status = 302 ) {
 
-          // Le routeur est sur $this->router, facile.
           $uri = $this->router->unroute($ruleId, $data, $secured);
-          var_dump($uri);
 
-          // Où est la réponse HTTP ? Dur à dire.
-          // En fait, la vue va s'écrire sur un flux qui est le flux sortant.
-          // Dans le cas d'une appli Web, ce sera notre réponse HTTP !
           $response = $this->view->getOutputStream();
           $response->sendHeader('Location', $uri, true, $status);
 
