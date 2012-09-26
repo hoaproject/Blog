196f9602bcb2866e6221e2bba3a1c67970aa28fc
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2012-09-26 20:01:11 +0200

    Update Data/ according to last Hoa\Data revision.

--- Data/Etc/Configuration/.Cache/HoaCoreCore.php
+++ Data/Etc/Configuration/.Cache/HoaCoreCore.php
@@ -7,30 +7,21 @@
 return array (
   'keywords' =>
   array (
-    'hoa' => NULL,
+    'root' => NULL,
     'cwd' => NULL,
   ),
   'parameters' =>
   array (
+    'root.hoa' => '(:root:)',
     'root.application' => '(:cwd:h:)',
     'root.data' => '(:%root.application:h:)/Data',
-    'root.hoa' => '/usr/local/lib/hoa',
-
-    'hoa.bin' => '(:%root.hoa:)/Core/Bin',
-    'hoa.core' => '(:%root.hoa:)/Core',
-    'hoa.library' => '(:%root.hoa:)/Library',
-    'hoa.module' => '(:%root.hoa:)/Module',
-
-    'data.module' => '(:%root.data:)/Module',
-
     'protocol.Application' => '(:%root.application:)/',
     'protocol.Application/Public' => 'Public/',
     'protocol.Data' => '(:%root.data:)/',
     'protocol.Data/Etc' => 'Etc/',
     'protocol.Data/Etc/Configuration' => 'Configuration/',
     'protocol.Data/Etc/Locale' => 'Locale/',
-    'protocol.Data/Etc/Library' => 'Library/Hoathis/;' .
-                                   'Library/Hoa/',
+    'protocol.Data/Library' => 'Library/Hoathis/;Library/Hoa/',
     'protocol.Data/Lost+found' => 'Lost+found/',
     'protocol.Data/Temporary' => 'Temporary/',
     'protocol.Data/Variable' => 'Variable/',
@@ -40,14 +31,8 @@ return array (
     'protocol.Data/Variable/Private' => 'Private/',
     'protocol.Data/Variable/Run' => 'Run/',
     'protocol.Data/Variable/Test' => 'Test/',
-    'protocol.Library' => '(:%protocol.Data:)Library/Hoathis/;' .
-                          '(:%protocol.Data:)Library/Hoa/;' .
-                          '(:%root.hoa:)/Hoathis/;' .
-                          '(:%root.hoa:)/Hoa/',
-
-    'namespace.prefix' => '(:%protocol.Data:)Library/;(:%root.hoa:)/',
-    'namespace.prefix.Hoa' => '(:%hoa.library:)',
-    'namespace.prefix.Hoathis' => '(:%data.module:);(:%hoa.module:)',
-    'namespace.prefix.Application' => '(:%root.application:)',
+    'protocol.Library' => '(:%protocol.Data:)Library/Hoathis/;(:%protocol.Data:)Library/Hoa/;(:%root.hoa:)/Hoathis/;(:%root.hoa:)/Hoa/'
+    'namespace.prefix.*' => '(:%protocol.Data:)Library/;(:%root.hoa:)/',
+    'namespace.prefix.Application' => '(:%root.application:h:)/',
   ),
 );
