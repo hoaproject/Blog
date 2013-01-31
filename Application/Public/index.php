96024771aaf244cb0b778313004ee7777a3dc12f
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-01-31 12:17:36 +0100

    Restore links.

--- Application/Public/index.php
+++ Application/Public/index.php
@@ -40,7 +40,13 @@ $router->get('posts',       '/',                               'posts', 'index')
        ->post('update_post','/admin/posts/(?<id>\d+)',         'admin\posts', 'update')
        ->get('delete_post', '/admin/posts/(?<id>\d+)/delete',  'admin\posts', 'delete')
 
-       ->_get('_resource', 'http://static.hoa-project.net/(?<resource>)');
+       ->_get('_resource', 'http://static.hoa-project.net/(?<resource>)')
+       ->_get('g',     '/',                null, null, array('_subdomain' => '__root__'))
+       ->_get('s',     '/Source.html',     null, null, array('_subdomain' => '__root__'))
+       ->_get('l',     '/Literature.html', null, null, array('_subdomain' => '__root__'))
+       ->_get('ev',    '/Event.html',      null, null, array('_subdomain' => '__root__'))
+       ->_get('forum', 'http://forum.hoa-project.net')
+       ->_get('c',     '/Contact.html',    null, null, array('_subdomain' => '__root__'));
 
 try {
 
