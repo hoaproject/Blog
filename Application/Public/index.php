6846357b1fae321e7086833dca7c035307512bb9
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-01-31 11:54:31 +0100

    Use Hoa's website design.

--- Application/Public/index.php
+++ Application/Public/index.php
@@ -38,7 +38,9 @@ $router->get('posts',       '/',                               'posts', 'index')
        ->post('create_post','/admin/posts/create',             'admin\posts', 'create')
        ->get('edit_post',   '/admin/posts/(?<id>\d+)/edit',    'admin\posts', 'edit')
        ->post('update_post','/admin/posts/(?<id>\d+)',         'admin\posts', 'update')
-       ->get('delete_post', '/admin/posts/(?<id>\d+)/delete',  'admin\posts', 'delete');
+       ->get('delete_post', '/admin/posts/(?<id>\d+)/delete',  'admin\posts', 'delete')
+
+       ->_get('_resource', 'http://static.hoa-project.net/(?<resource>)');
 
 try {
 
